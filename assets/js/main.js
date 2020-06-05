b0158be4e119128cdec13179a249edd4a9dac393
AlexPullen91 <alex_pullen03@hotmail.com>
2020-06-05 01:10:31 +0000

    experimental local question loading code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,54 @@ const trueButton = document.getElementById("trueButton");
 const falseButton = document.getElementById("falseButton");
 
 
+let currentStatement = {};
+
+let availableStatements = [];
+
+let statements = [
+  {
+    category: "Animals",
+    type: "boolean",
+    difficulty: "medium",
+    question: "An octopus can fit through any hole larger than its beak.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "General Knowledge",
+    type: "boolean",
+    difficulty: "easy",
+    question: "French is an official language in Canada.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Entertainment: Musicals & Theatres",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "In the play Oedipus Rex, Oedipus kills his father due to jealousy in loving his mother.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+];
+
+
+startGame = () => {
+
+    availableStatements = [...statements];
+    // console.log(availableStatements);
+    getNextStatement();
+};
+
+getNextStatement = () => {
+  const statementIndex = Math.floor(Math.random() * availableStatements.length);
+  currentStatement = availableStatements[statementIndex];
+  statement.innerText = currentStatement.question;
+
+  correctAnswer = availableStatements[0].correct_answer;
+  
+  trueButtom = correctAnswer;
+};
+
+startGame();
