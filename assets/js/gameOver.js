77144a3ff084cf74f181f5f17d717d3f45b70ffc
AlexPullen91 <alex_pullen03@hotmail.com>
2020-06-14 13:43:55 +0000

    high scores retrieved from local storage and converted into array object

--- assets/js/gameOver.js
+++ assets/js/gameOver.js
@@ -1,2 +1,4 @@
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 score.innerText = `You scored: ${mostRecentScore}`;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
