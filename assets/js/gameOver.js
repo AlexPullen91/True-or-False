49fe5a9b6681d2cb7110cc36f193acd12f2d081b
AlexPullen91 <alex_pullen03@hotmail.com>
2020-06-14 14:40:37 +0000

    added function to save high scores

--- assets/js/gameOver.js
+++ assets/js/gameOver.js
@@ -2,3 +2,12 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 score.innerText = `You scored: ${mostRecentScore}`;
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+saveHighScore = (e) => {
+    e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    console.log(score);
+}
