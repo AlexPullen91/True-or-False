bc22328bae165f028345cabc6b37ac01190fd3d9
AlexPullen91 <alex_pullen03@hotmail.com>
2020-06-15 13:41:44 +0000

    added comments to high scores js

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -1,6 +1,10 @@
-const highScoresList = document.querySelector("#highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const highScoresResults = document.querySelector("#highScoresResults");
 
-highScoresList.innerHTML = highScores.map(score => {
+/**
+ * iterates through each score in highscores array and 
+ * and adds an <li></li> to the list
+ */
+highScoresResults.innerHTML = highScores.map(score => {
     return `<li class="high-score-list-items">${score.name} - ${score.score}</li>`;
 }).join("")
